dad9e859285dbbab0121b3d50ca21816db4d4bb5
ali emami <alisabouri6857@gmail.com>
2025-12-15 09:37:24 +0000

    test demo

---
+++ src/COMP/RCMP_reactCarousel/styles/navigation/styles.module.css.d.ts
@@ -0,0 +1,8 @@
+
+export const carouselNavigation: string = "";
+interface Namespace {
+	carouselNavigation: string;
+	'carousel-navigation': string;
+}
+declare const stylesModule: Namespace;
+export default stylesModule;
---
+++ src/COMP/RCMP_root/index.tsx
@@ -0,0 +1,254 @@
+import { useState } from "react";
+
+import {
+  AddCircle,
+  ArrowRotateRight,
+  CloseCircle,
+  Sort,
+  TagRight,
+  AddSquare,
+  Data2,
+} from "iconsax-react";
+
+import classNames from "classnames";
+
+interface Dimention {
+  Meta: any;
+
+  packetID?: string;
+  assignType?: string;
+
+  // Control Parameters for paraWidgs
+  editable?: boolean;
+  staticEditable?: boolean;
+  paraPick?: boolean;
+  hide?: boolean;
+  required: boolean;
+
+  sort?: string;
+  filter?: string;
+  search?: string;
+  autoSave?: boolean;
+  machinSerial?: string;
+}
+
+function Index(dimention: Dimention) {
+  const [packetID, setPacketID] = useState<any>("");
+  const [isOpen, setIsOpen] = useState(false);
+  const [config, setConfig] = useState<any>({
+    value: "Root",
+    icon: false,
+    changeScale: false,
+    resetOption: false,
+  });
+
+  // useEffect(() => {
+  //     dimention.packetID = packetID;
+  //     dimention.assignType = assignType;
+  // }, [packetID, assignType])
+
+  const status = "config";
+
+  return (
+    <div className="border-2 w-full flex flex-col rounded-xl">
+      <div className="flex items-center justify-between font-bold bg-gray-100 p-4">
+        <div
+          className={classNames({
+            relative: true,
+          })}
+        >
+          <div
+            onClick={() => setIsOpen(!isOpen)}
+            className={classNames({
+              "flex gap-3 ": true,
+              "cursor-pointer": status === "config",
+            })}
+          >
+            <Data2 size={24} color="currentColor" />
+            <span className="font-bold">{config.defVal}</span>
+          </div>
+          {status === "config" && isOpen ? (
+            <div className="z-[999] absolute top-6 left-0 border shadow-lg rounded-lg w-fit bg-white flex flex-col gap-3 pb-4">
+              <div className="w-full flex items-center justify-end px-4 pt-2">
+                <CloseCircle
+                  size={24}
+                  color="currentColor"
+                  className="cursor-pointer text-gray-500"
+                  onClick={() => setIsOpen(!isOpen)}
+                />
+              </div>
+              <div className="flex items-center justify-start gap-2 px-4 w-full">
+                <span className="block w-fit truncate text-sm text-gray-900">
+                  Default Value:
+                </span>
+                <input
+                  type="text"
+                  id="defVal"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-fit h-8 p-2.5"
+                  value={config.defVal}
+                  onChange={(e) =>
+                    setConfig({ ...config, value: e.target.value })
+                  }
+                />
+              </div>
+              <div className="flex items-center justify-between gap-2 px-4 py-2 bg-gray-50 border-l-8 border-blue-500">
+                <label className="block w-fit text-sm text-gray-900">
+                  Icon
+                </label>
+                <label className="relative inline-flex items-center cursor-pointer w-fit">
+                  <input
+                    onChange={(e) =>
+                      setConfig({ ...config, icon: e.target.checked })
+                    }
+                    type="checkbox"
+                    value=""
+                    className="sr-only peer"
+                    checked={config.icon}
+                  />
+                  <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+                </label>
+              </div>
+              <div className="flex items-center justify-between gap-2 px-4">
+                <label className="block w-fit text-sm text-gray-900">
+                  Reset Option
+                </label>
+                <label className="relative inline-flex items-center cursor-pointer w-fit">
+                  <input
+                    onChange={(e) =>
+                      setConfig({ ...config, resetOption: e.target.checked })
+                    }
+                    type="checkbox"
+                    value=""
+                    className="sr-only peer"
+                    checked={config.resetOption}
+                  />
+                  <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+                </label>
+              </div>
+              <div className="flex items-center justify-between gap-2 px-4">
+                <label className="block w-fit text-sm text-gray-900">
+                  Change Scale
+                </label>
+                <label className="relative inline-flex items-center cursor-pointer w-fit">
+                  <input
+                    type="checkbox"
+                    className="sr-only peer"
+                    onChange={(e) =>
+                      setConfig({ ...config, changeScale: e.target.checked })
+                    }
+                    checked={config.changeScale}
+                  />
+                  <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+                </label>
+              </div>
+              <div className="flex items-center justify-between gap-2 px-4 w-full">
+                <span className="block w-fit truncate text-sm text-gray-900">
+                  Scale:
+                </span>
+                <select
+                  id="defVal"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-28 h-8 p-2.5"
+                >
+                  <option>---</option>
+                </select>
+              </div>
+              <div className="flex items-center justify-between gap-2 px-4 w-full">
+                <span className="block w-fit truncate text-sm text-gray-900">
+                  Format Default:
+                </span>
+                <select
+                  id="defVal"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-28 h-8 p-2.5"
+                >
+                  <option>---</option>
+                </select>
+              </div>
+              <div className="flex items-center justify-between gap-2 px-4 w-full">
+                <span className="block w-fit truncate text-sm text-gray-900">
+                  Valid Time:
+                </span>
+                <select
+                  id="defVal"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-28 h-8 p-2.5"
+                >
+                  <option>---</option>
+                </select>
+              </div>
+              <div className="flex items-center justify-between gap-2 px-4 w-full">
+                <span className="block w-fit truncate text-sm text-gray-900">
+                  Valid Formula:
+                </span>
+                <input
+                  type="text"
+                  id="defVal"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-28 h-8 p-2.5"
+                />
+              </div>
+              <div className="flex items-center justify-between gap-2 px-4 w-full">
+                <span className="block w-fit truncate text-sm text-gray-900">
+                  Widget Variant:
+                </span>
+                <input
+                  type="text"
+                  id="defVal"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-28 h-8 p-2.5"
+                />
+              </div>
+            </div>
+          ) : null}
+        </div>
+        <div className="flex gap-2">
+          {status === "config" ? (
+            <>
+              <Sort size={24} color="currentColor" />
+              <AddCircle size={24} color="currentColor" />
+              <CloseCircle size={24} color="currentColor" />
+            </>
+          ) : (
+            <>
+              <ArrowRotateRight size={24} color="currentColor" />
+              <TagRight size={24} color="currentColor" />
+            </>
+          )}
+        </div>
+      </div>
+      <div className="p-4">
+        <div className=" w-full flex flex-col justify-center items-center gap-3">
+          <div className="flex">
+            <div className=" w-0.5 h-[3rem] bg-black mt-[1.2rem] "></div>
+            <div className="flex flex-col gap-3">
+              <div>
+                <div className="flex items-center">
+                  <div className=" h-0.5 w-4 bg-black " />
+                  <input
+                    onChange={(e) => setPacketID(e.target.value)}
+                    value={packetID}
+                    type="text"
+                    id="Target"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="Root Name"
+                    required={dimention.required}
+                    autoFocus
+                    key={Math.random()}
+                  />
+                </div>
+              </div>
+              <div>
+                <div className="flex items-center">
+                  <div className=" h-0.5 w-4 bg-black " />
+                  <AddSquare
+                    className="text-gray-500"
+                    size={25}
+                    color="currentColor"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Index;
